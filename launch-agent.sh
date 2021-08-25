33b2863efb515082a3662c4c3cb0ee29967a3fd1
John Else <john.else@gmail.com>
2021-08-25 21:31:21 +0100

    Add script for launching an agent with a given name
    This script logs into jenkins, gets the agent secret, then launches an
    agent container using the given node name and secret.

---
+++ launch-agent.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eu
+
+if [ $# != 1 ]
+then
+    echo "Usage: $0 <node-name>"
+    exit 1
+fi
+
+NODE_NAME=$1
+CONTAINER_NAME=jenkins-node-${NODE_NAME}
+
+CREDENTIALS_FILE=credentials.sh
+if [ -f $CREDENTIALS_FILE ]
+then
+    . credentials.sh
+else
+    echo "Please create $CREDENTIALS_FILE first"
+fi
+
+AGENT_SECRET=$(curl -L -s -u $JENKINS_USERNAME:$JENKINS_PASSWORD -X GET http://localhost:8080/computer/${NODE_NAME}/jenkins-agent.jnlp \
+    | sed "s/.*<application-desc main-class=\"hudson.remoting.jnlp.Main\"><argument>\([a-z0-9]*\).*/\1/")
+
+make -C agent
+
+docker run -d --rm --name $CONTAINER_NAME \
+    --network jenkins \
+    -e AGENT_URL=http://jenkins:8080/computer/${NODE_NAME}/jenkins-agent.jnlp \
+    -e AGENT_SECRET=$AGENT_SECRET \
+    ${USER}/jenkins-agent /home/jenkins/launch.sh
