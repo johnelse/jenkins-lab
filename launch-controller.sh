71b874052966f2cfc79d19cabe09aa74b95241b4
John Else <john.else@gmail.com>
2021-08-25 21:26:04 +0100

    Connect jenkins controller to new jenkins network

--- launch-controller.sh
+++ launch-controller.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+docker network inspect jenkins || docker network create jenkins
+
 docker run -d --rm -p 8080:8080 -p 50000:50000 \
+    --network jenkins \
     -v jenkins_home:/var/jenkins_home \
     --name jenkins \
     jenkins/jenkins:lts
