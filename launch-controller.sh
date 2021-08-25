62d778613a4b95833bde29af87ed150844b93c51
John Else <john.else@gmail.com>
2021-08-25 21:38:39 +0100

    Have the controller listen on 127.0.0.1 only

--- launch-controller.sh
+++ launch-controller.sh
@@ -2,7 +2,7 @@
 
 docker network inspect jenkins || docker network create jenkins
 
-docker run -d --rm -p 8080:8080 -p 50000:50000 \
+docker run -d --rm -p 127.0.0.1:8080:8080 -p 127.0.0.1:50000:50000 \
     --network jenkins \
     -v jenkins_home:/var/jenkins_home \
     --name jenkins \
